94febf633e369e228593440a2f5c302e8e48cae6
Yutan Zhang <ytzh@ucdavis.edu>
2023-06-05 14:43:34 -0700

    diagonalization part works

--- src/diagonalize_all_clusters_xxz.jl
+++ src/diagonalize_all_clusters_xxz.jl
@@ -4,22 +4,25 @@
     By default we set J_xy=1.0 and J_z=J_z/J_xy. Results for Different J_z could be extracted by multiplying E by J_xy.
 """
 function diagonalize_all_clusters_xxz(; J_xy::Float64, J_z::Float64, Nmax::Int64, clusters_info_path::String, diag_folder_path::String)
-    # Maximum NLCE order and
-    # J1 and J2 values are input arguments
 
-    # Holding "lattice constants" (multiplicities) for all clusters
-    # in all orders. Probably good for up to order 9. Need to check
-    # what is needed exactly for the size of the second dimension.
-    multi = zeros(Int64, Nmax + 1, 16000)
+    # ===================================================#
+    # ======      load up cluster information      ======#
+    # ===================================================#
 
-    # Holding the two site numbers and optionally the bond type
-    # for every bond.
-    site1 = zeros(Int64, 40)
-    site2 = zeros(Int64, 40)
-    typ = zeros(Int64, 40)
+    # load up all the clusters' hash tags to `cluster_hash_tags`
+    # load up bond information to 'bonds'
+    cluster_hash_tags = []
+    bonds = Dict{String,Vector{Vector{Int64}}}()
+    for N in 1:Nmax
+        cluster_bond_info_fname = joinpath(clusters_info_path, "graph_bond_triangle_" * string(N) * ".json")
 
-    # Counter for isomorphically distinct clusters
-    topN = 0
+        bonds_N = JSON.parsefile(cluster_bond_info_fname)
+
+        merge!(bonds, bonds_N)
+        println("Finished read in bond info of order $(N)")
+
+        push!(cluster_hash_tags, keys(bonds_N))
+    end
 
     # prefix of files to hold eigenvalues
     name = "xxz_eigvals"
@@ -27,81 +30,50 @@ function diagonalize_all_clusters_xxz(; J_xy::Float64, J_z::Float64, Nmax::Int64
     # Loop over the NLCE order
     for N in 2:Nmax
 
-        println("starting order " * string(N))
+        println("starting diagonalizing order " * string(N))
 
         # Generate sector info for julia script
         sectors_info = sectors_info_gen(N=N)
 
-        # Change 1 to 2 in the line below and in the next cell to include n.n.n. bonds
-        fname = joinpath(clusters_info_path, "NLCE_1_" * string(N) * ".txt")
-        file = open(fname, "r")
+        # the file to hold the eigen values 
+        diag_name = "diagonalized_order$(N)"
+        diag_file = joinpath(diag_folder_path, diag_name * ".jld2")
 
-        # name of the file storing diagonalization result
-        diag_name = "diagonalized"
+        # collection of all the clusters' hash tags of the Nth order
+        cluster_hash_tags_N = cluster_hash_tags[N]
 
-        # Skips line with order number
-        readline(file)
+        # total number of clusters at this order
+        tot_num_clusters = length(cluster_hash_tags_N)
 
-        # Going through the file for each order line by line and reading in the cluster information
-        topN = parse(Int64, readline(file))
+        # loop over all clusters in the Nth order
+        for (cluster_ind, cluster_hash_tags) in enumerate(cluster_hash_tags_N)
 
-        EOF = false
-        while EOF == false
+            # =======================================
+            # ======= diagonalize the cluster =======
+            # =======================================
 
-            # passing topN to julia script
-            NTOP = topN + 1
-            if mod(topN, 1000) == 0
-                println("diagonalizing order" * string(N) * "topo#" * string(topN))
+            # read in bond information
+            # since bond info start to index at 0, we need to plus 1
+            cluster_bonds = [[bond[1] + 1, bond[2] + 1] for bond in bonds[cluster_hash_tags]]
+
+            # diagonalize, get eigen values
+            quantities = diagonalize_cluster_xxz(N=N, sectors_info=sectors_info, bonds=cluster_bonds, J_xy=1.0, J_z=J_z / J_xy)
+
+            # save eigen values to file
+            # only save E and M for each state to save space
+            save(
+                diag_file,
+                cluster_hash_tags,
+                [quantities[1], quantities[3]]
+            )
+
+            # progress checker
+            if mod(cluster_ind, 1000) == 0
+                println("finished T averaging #$(cluster_ind) of order $(N)")
             end
 
-            line = split(readline(file))
-
-            # Get the number of bonds from this line, and pass it to julia script
-            nB = parse(Int64, line[1])
-
-            # Read in the bond information, and pass it to julia script
-            bonds = Array{Int64,1}[]
-            for b in 1:nB
-                line = split(readline(file))
-                # typ[b] = parse(Int64, line[3])
-                site1[b] = parse(Int64, line[1])
-                site2[b] = parse(Int64, line[2])
-                push!(bonds, [site1[b] + 1, site2[b] + 1])
-            end
-
-            # calculate eigenvalues of matrix and expectations of quantities in eigenstates. By default we set J_xy=1.0 and J_z=J_z/J_xy. Results for Different J_z could be extracted by multiplying E by J_xy.
-            quantities = diagonalize_cluster_xxz(N=N, sectors_info=sectors_info, bonds=bonds, J_xy=1.0, J_z=J_z / J_xy)
-
-            # printing diagonalization data to a file for save
-            printing(quantities; name=diag_name, NTOP=topN, N=N, folder_path=diag_folder_path)
-
-            # ---------------------------------------------------------
-
-            # Here, we take the opportunity to read in the "lattice constants"
-            # (multiplicities) for each topological cluster if we have reached
-            # the end of the file.
-
-            # skipping the subgraph information for now
-            sc_Num = parse(Int64, readline(file))
-            for s in 1:sc_Num
-                readline(file)
-            end
-            readline(file)
-
-            # Checking if we have reached the end of the file
-            if occursin("#    Topo.    #    :    Multp. ", readline(file))
-                # next(file)
-                for i in 1:(topN+1)
-                    multi[N, i] = parse(Int64, split(readline(file))[2])
-                end
-                EOF = true
-                close(file)
-                break
-            else
-                # If not yet done with clusters
-                topN += 1
-            end
         end
+
         println("finished diagonalizing clusters of order " * string(N))
     end
 end
